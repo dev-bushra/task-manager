64ea8e3ae51ac25f744d257fa49f5db8d0f301eb
Bushra Mohammed <bushra.ebox@gmail.com>
2025-06-11 06:19:16 +0400

    🔁 Update component to work with HTTP service

--- frontend/src/app/components/task-list/task-list.component.ts
+++ frontend/src/app/components/task-list/task-list.component.ts
@@ -1,31 +1,43 @@
-import { Component } from '@angular/core';
+// 🔁 Update component to work with HTTP service
+
+import { Component, OnInit } from '@angular/core';
 import { TaskService } from '../../services/task.service';
+import { Task } from '../../models/task.model';
 
 @Component({
   selector: 'app-task-list',
   templateUrl: './task-list.component.html',
 })
-
-export class TaskListComponent {
+export class TaskListComponent implements OnInit {
   newTask = '';
-  tasksObservable$ = this.taskService.tasksObservable$;
+  tasks: Task[] = [];
 
-  constructor(public taskService: TaskService) {}
+  constructor(private taskService: TaskService) {}
+
+  ngOnInit(): void {
+    this.taskService.getTasks().subscribe((data) => (this.tasks = data));
+  }
 
   addTask() {
     const task = this.newTask.trim();
     if (task) {
-      this.taskService.addTask(task);
-      this.newTask = '';
+      this.taskService.addTask(task).subscribe((newTask) => {
+        this.tasks.push(newTask);
+        this.newTask = '';
+      });
     }
   }
 
   toggleTask(id: number) {
-    this.taskService.toggleTask(id);
+    this.taskService.toggleTask(id).subscribe((updated) => {
+      const index = this.tasks.findIndex((t) => t.id === id);
+      if (index !== -1) this.tasks[index] = updated;
+    });
   }
 
   deleteTask(id: number) {
-    this.taskService.removeTask(id);
+    this.taskService.removeTask(id).subscribe(() => {
+      this.tasks = this.tasks.filter((task) => task.id !== id);
+    });
   }
-  
 }
