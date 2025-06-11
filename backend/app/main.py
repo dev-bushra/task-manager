a0c23255c751ede99f7e901594129067123000f4
Bushra Mohammed <bushra.ebox@gmail.com>
2025-06-11 06:15:56 +0400

    CROS added

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,21 @@
 from fastapi import FastAPI
 from .database import engine, metadata
 from .routers import tasks
+from fastapi.middleware.cors import CORSMiddleware
 
 # 🔨 Create all tables at startup
 metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+#  👇 أضف الكود دا مباشرة بعد إنشاء app
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:4200"],  # هنا تحط عنوان الـ frontend بتاعك
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # ✅ API Routers
 app.include_router(tasks.router, prefix="/tasks", tags=["Tasks"])
